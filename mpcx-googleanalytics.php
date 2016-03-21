a473f6ecee33c5c345f8270516d0bf09a5a1563a
Stefan Hüsges <tronsha@gmx.de>
2016-03-21 09:24:14 +0100

    added output

--- mpcx-googleanalytics.php
+++ mpcx-googleanalytics.php
@@ -35,9 +35,39 @@ function mpcx_google_analytics_add_options_page() {
 	);
 }
 
+function mpcx_google_analytics_activate() {
+	add_option('google_analytics_id', 'UA-0000000-0');
+}
+
+function mpcx_google_analytics_deactive() {
+	delete_option('google_analytics_id');
+}
+
 function mpcx_google_analytics_admin_options() {
 	include plugin_dir_path( __FILE__ ) . 'admin/options.php';
 }
+
+function mpcx_google_analytics_get_option() {
+	return get_option('google_analytics_id');
+}
+
+function mpcx_google_analytics_output() {
+	$id = mpcx_google_analytics_get_option();
+	echo "
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', '" . $id . "', 'auto');
+  ga('set', 'anonymizeIp', true);
+  ga('send', 'pageview');
+</script>
+";
+}
+
+register_activation_hook(__FILE__, 'mpcx_google_analytics_activate');
+register_deactivation_hook(__FILE__, 'mpcx_google_analytics_deactive');
 
 if ( is_admin() ) {
 	add_action( 'admin_init', 'mpcx_google_analytics_register_setting' );
@@ -45,4 +75,5 @@ if ( is_admin() ) {
 }
 
 if ( ! is_admin() ) {
+	add_action('wp_head', 'mpcx_google_analytics_output');
 }
