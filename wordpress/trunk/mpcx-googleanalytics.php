bd59f115e9f6228cd8d0cdc0ba9f61e863756fa3
Stefan Hüsges <tronsha@gmx.de>
2016-03-23 15:42:38 +0100

    moved script to template file

--- wordpress/trunk/mpcx-googleanalytics.php
+++ wordpress/trunk/mpcx-googleanalytics.php
@@ -8,7 +8,7 @@
  * Plugin Name:       Google Analytics
  * Plugin URI:        https://github.com/tronsha/wp-googleanalytics-plugin
  * Description:       Google Analytics with Anonymize IP.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Stefan Hüsges
  * Author URI:        http://www.mpcx.net/
  * Copyright:         Stefan Hüsges
@@ -86,17 +86,11 @@ if ( ! is_admin() ) {
 		function () {
 			$trackingId = get_option( 'google_analytics_tracking_id' );
 			if ( empty( $trackingId ) === false && $trackingId !== 'UA-0000000-0' ) {
-				echo "
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', '" . $trackingId . "', 'auto');
-  ga('set', 'anonymizeIp', true);
-  ga('send', 'pageview');
-</script>
-";
+				echo str_replace(
+					'%trackingId%',
+					$trackingId,
+					file_get_contents( plugin_dir_path( __FILE__ ) . 'script.tpl' )
+				);
 			}
 		}
 	);
